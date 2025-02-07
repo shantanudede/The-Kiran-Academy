56d5adc456d76c11fb8cc6a833c6bde4d2f6156e
Shantanu Jitesh Dede <128113090+shantanudede@users.noreply.github.com>
2025-02-07 16:46:39 +0530

    Create HomeWork6febQ1.java

---
+++ HomeWork6febQ1.java
@@ -0,0 +1,39 @@
+package org.tka.loops;
+
+public class HomeWork6febQ1 {
+	
+	public static void main(String[] args) {
+		
+int a =1;
+		
+		System.out.println("******BY FOR LOOP******");
+				
+				for(a=1;a<=10;a++)
+					System.out.println(a);
+				
+		System.out.println("-------------------------");
+		
+		System.out.println("------BY WHILE LOOP------");
+		
+		int c=1;
+		
+		while(c<=10) {
+			System.out.println(c);
+			c++;
+			
+		}
+		
+		System.out.println("-------------------------");
+
+		
+		System.out.println("------BY DO WHILE LOOP------");
+		
+		int b =1;
+			do {
+				System.out.println(b);
+				b++;
+			}while(b<=10);
+
+	}
+
+}
